821c3ae37cf18b66d07779a2a898e62997a3e3f8
Christian Rebelsky <christian.rebelsky@gmail.com>
2024-06-18 09:56:28 +0200

    remove remote patterns for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
